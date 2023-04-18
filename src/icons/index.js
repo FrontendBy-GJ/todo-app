9bbfb74c350771a3677394a687db6b47c03f95f8
Genaro Garcia <genaro.dev@outlook.com>
2023-04-18 15:12:38 -0500

    initial commit

---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+export { default as SunIcon } from './SunIcon';
+export { default as MoonIcon } from './MoonIcon';
+export { default as CrossIcon } from './CrossIcon';
+export { default as CheckIcon } from './CheckIcon';
