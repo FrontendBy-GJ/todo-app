f983adc73fe4aa727a354ea7ee3193cb526095cf
Genaro Garcia <genaro.dev@outlook.com>
2023-04-21 12:50:39 -0500

    change background img on theme toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import desktopLight from '../assets/bg-desktop-light.jpg';
 import mobileLight from '../assets/bg-mobile-light.jpg';
+import desktopDark from '../assets/bg-desktop-dark.jpg';
+import mobileDark from '../assets/bg-mobile-dark.jpg';
 import { MoonIcon, SunIcon } from '../icons';
 
 const Header = ({ darkMode, toggleTheme }) => {
   return (
     <header>
       <picture>
-        <source media="(max-width: 600px)" srcSet={mobileLight} sizes="375px" />
-        <source srcSet={desktopLight} sizes="1440px" />
+        <source
+          media="(max-width: 600px)"
+          srcSet={darkMode ? mobileDark : mobileLight}
+          sizes="375px"
+        />
+        <source srcSet={darkMode ? desktopDark : desktopLight} sizes="1440px" />
         <img src={mobileLight} alt="mountains" className="absolute w-full" />
       </picture>
       <section className="flex justify-between pt-10 lg:pt-14 max-w-md mx-auto relative px-4 xl:max-w-xl xl:pt-20">
